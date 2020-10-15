b89e989d814e7496776df48bc679594277750edb
rarcz29 <rafalcz29@gmail.com>
2020-10-15 16:49:11 +0200

    Create MVVM structure

---
+++ Saper.Presentation/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using Saper.Presentation.ViewModels;
+using System.Windows;
+
+namespace Saper.Presentation
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Saper.Presentation/ViewModels/ShellViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Caliburn.Micro;
+
+namespace Saper.Presentation.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+    }
+}
