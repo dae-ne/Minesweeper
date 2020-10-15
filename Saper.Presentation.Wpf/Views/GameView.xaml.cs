e3bacc35baf089795821b60449e9366d11f12430
rarcz29 <rafalcz29@gmail.com>
2020-10-15 21:51:26 +0200

    Update views

--- Saper.Presentation.Wpf/Views/GameView.xaml.cs
+++ Saper.Presentation.Wpf/Views/GameView.xaml.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MvvmCross.Platforms.Wpf.Views;
 
 namespace Saper.Presentation.Wpf.Views
 {
     /// <summary>
     /// Interaction logic for GameView.xaml
     /// </summary>
-    public partial class GameView : UserControl
+    public partial class GameView : MvxWpfView
     {
         public GameView()
         {
--- Saper.Presentation.Wpf/Views/MenuView.xaml.cs
+++ Saper.Presentation.Wpf/Views/MenuView.xaml.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MvvmCross.Platforms.Wpf.Views;
 
 namespace Saper.Presentation.Wpf.Views
 {
     /// <summary>
     /// Interaction logic for MenuView.xaml
     /// </summary>
-    public partial class MenuView : UserControl
+    public partial class MenuView : MvxWpfView
     {
         public MenuView()
         {
