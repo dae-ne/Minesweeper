fcafb43d429841e95dd2d8241c844d11a2a8caa3
rarcz29 <rafalcz29@gmail.com>
2020-10-15 23:44:19 +0200

    Update navigation

--- Minesweeper.Presentation.Core/ViewModels/MenuViewModel.cs
+++ Minesweeper.Presentation.Core/ViewModels/MenuViewModel.cs
@@ -1,8 +1,41 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using System.Threading.Tasks;
 
 namespace Minesweeper.Presentation.Core.ViewModels
 {
     class MenuViewModel : MvxViewModel
     {
+        private readonly IMvxNavigationService _navigationService;
+
+        public IMvxAsyncCommand NewGameEasyCommand { get; set; }
+
+        public IMvxAsyncCommand NewGameNormalCommand { get; set; }
+
+        public IMvxAsyncCommand NewGameHardCommand { get; set; }
+
+        public MenuViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            NewGameEasyCommand = new MvxAsyncCommand(NewGameEasy);
+            NewGameNormalCommand = new MvxAsyncCommand(NewGameNormal);
+            NewGameHardCommand = new MvxAsyncCommand(NewGameHard);
+        }
+
+        private async Task NewGameEasy()
+        {
+            await _navigationService.Navigate<GameViewModel>();
+        }
+
+        private async Task NewGameNormal()
+        {
+            await _navigationService.Navigate<GameViewModel>();
+        }
+
+        private async Task NewGameHard()
+        {
+            await _navigationService.Navigate<GameViewModel>();
+        }
     }
 }
