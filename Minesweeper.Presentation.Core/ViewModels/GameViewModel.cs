7e2f29f1e99b2d36c31805feca133f194da21489
rarcz29 <rafalcz29@gmail.com>
2020-10-15 23:59:10 +0200

    Update

--- Minesweeper.Presentation.Core/ViewModels/GameViewModel.cs
+++ Minesweeper.Presentation.Core/ViewModels/GameViewModel.cs
@@ -1,8 +1,25 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using System.Threading.Tasks;
 
 namespace Minesweeper.Presentation.Core.ViewModels
 {
     class GameViewModel : MvxViewModel
     {
+        private readonly IMvxNavigationService _navigationService;
+
+        public IMvxAsyncCommand GoBackCommand { get; set; }
+
+        public GameViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            GoBackCommand = new MvxAsyncCommand(GoBack);
+        }
+
+        private async Task GoBack()
+        {
+            await _navigationService.Close(this);
+        }
     }
 }
