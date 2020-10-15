65cab9a4092f9e1e003b8289d2817a5215e572d5
rarcz29 <rafalcz29@gmail.com>
2020-10-15 22:10:56 +0200

    Update

--- Saper.Presentation.Wpf/MainWindow.xaml.cs
+++ Saper.Presentation.Wpf/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System.Windows;
+﻿using MvvmCross.Platforms.Wpf.Views;
 
 namespace Saper.Presentation.Wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MvxWindow
     {
         public MainWindow()
         {
