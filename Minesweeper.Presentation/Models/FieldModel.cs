5c8a916a186fa053869563b35257995fcaf03cb2
rarcz29 <rafalcz29@gmail.com>
2020-10-19 16:22:09 +0200

    Update

---
+++ Minesweeper.Presentation/Models/FieldModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Minesweeper.Presentation.Models
+{
+    class FieldModel
+    {
+        public string Text { get; set; }
+
+        public bool IsEnabled { get; set; } = true;
+    }
+}
--- Minesweeper.Presentation/ViewModels/GameViewModel.cs
+++ Minesweeper.Presentation/ViewModels/GameViewModel.cs
@@ -1,19 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Caliburn.Micro;
+using Minesweeper.Presentation.Models;
 
 namespace Minesweeper.Presentation.ViewModels
 {
-    class GameViewModel
+    class GameViewModel : Conductor<object>
     {
+        private const string _MenuButtonText = "Start Game";
+        private const string _GameButtonText = "Stop";
+
+        private BindableCollection<FieldModel> _fields;
+
+        public BindableCollection<FieldModel> Fields
+        {
+            get { return _fields; }
+            set
+            {
+                _fields = value;
+                NotifyOfPropertyChange(() => Fields);
+            }
+        }
+
         private double _windowWidth;
 
         public double WindowWidth
         {
             get { return _windowWidth; }
-            set { _windowWidth = value; }
+            set
+            {
+                _windowWidth = value;
+                NotifyOfPropertyChange(() => WindowWidth);
+                NotifyOfPropertyChange(() => FieldWidth);
+            }
         }
 
         private double _windowHeight;
@@ -21,13 +38,100 @@ namespace Minesweeper.Presentation.ViewModels
         public double WindowHeight
         {
             get { return _windowHeight; }
-            set { _windowHeight = value; }
+            set
+            {
+                _windowHeight = value;
+                NotifyOfPropertyChange(() => WindowHeight);
+                NotifyOfPropertyChange(() => FieldHeight);
+            }
         }
 
-        public void DoSomething()
+        private string _navButtonText;
+
+        public string NavButtonText
         {
-            WindowWidth = 100.0;
-            WindowHeight = 100.0;
+            get { return _navButtonText; }
+            set
+            {
+                _navButtonText = value;
+                NotifyOfPropertyChange(() => NavButtonText);
+            }
+        }
+
+        public double FieldWidth
+        {
+            get { return (WindowWidth - 215.0) / 12.0; }
+        }
+
+        public double FieldHeight
+        {
+            get { return WindowHeight / 8.0; }
+        }
+
+        public GameViewModel()
+        {
+            NavButtonText = _MenuButtonText;
+
+            Fields = new BindableCollection<FieldModel>();
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "asdf", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+            Fields.Add(new FieldModel { Text = "", IsEnabled = true });
+        }
+
+        public void NavigateClick()
+        {
+            if (NavButtonText == _MenuButtonText)
+            {
+                NavButtonText = _GameButtonText;
+            }
+            else
+            {
+                NavButtonText = _MenuButtonText;
+            }
+        }
+
+        public void DoSomething(FieldModel field)
+        {
+            var index = Fields.IndexOf(field);
+            Fields.RemoveAt(index);
+            Fields.Insert(index, new FieldModel { Text = "1", IsEnabled = false });
         }
     }
 }
